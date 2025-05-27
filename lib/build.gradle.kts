13d4d9580fcdd404e57fdb8a3f298d8a42dca251
seazon <dxdroid@gmail.com>
2025-05-27 08:29:05 +0800

    fix summary parse issue

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.seazon.feedme"
-version = "0.35"
+version = "0.48"
 
 publishing {
     repositories {
--- lib/src/main/kotlin/com/seazon/feedme/lib/ai/gemini/GeminiApi.kt
+++ lib/src/main/kotlin/com/seazon/feedme/lib/ai/gemini/GeminiApi.kt
@@ -3,9 +3,11 @@ package com.seazon.feedme.lib.ai.gemini
 import com.seazon.feedme.lib.network.HttpManager
 import com.seazon.feedme.lib.network.HttpMethod
 import com.seazon.feedme.lib.network.NameValuePair
+import com.seazon.feedme.lib.network.toType
 import com.seazon.feedme.lib.utils.toJson
-import io.ktor.client.call.body
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class GeminiApi {
 
@@ -24,16 +26,26 @@ class GeminiApi {
     }
 
     suspend fun summary(query: String, language: String, key: String): Translation? {
-        val result = generateContent("Summary the text in $language, JSON format output, key is dst. Text: $query", key)
+        val result = generateContent("Summary the text in $language, JSON format output, key is dst, dst should be a string, no more than 500 words, use markdown to improve readability if need. Text: $query", key)
         val text = result?.candidates?.firstOrNull()?.content?.parts?.firstOrNull()?.text
         return if (text.isNullOrEmpty()) {
             null
         } else {
-            toJson<Translation>(text.replace("```json\n", "").replace("```", ""))
+            toJson<Translation>(text.replace("```json\n", "").replace("\n```", ""))
         }
     }
 
     private suspend fun generateContent(prompt: String, key: String): Result? {
+        val requestBody = RequestBody(
+            listOf(
+                Content(
+                    listOf(
+                        Part(prompt),
+                    ),
+                ),
+            ),
+        )
+        val body = Json.encodeToString(requestBody).trimIndent()
         return HttpManager.request(
             httpMethod = HttpMethod.POST,
             url = URL,
@@ -43,15 +55,15 @@ class GeminiApi {
             params = listOf(
                 NameValuePair("key", key),
             ),
-            body = """{
-    "contents": [{
-    "parts":[{"text": "$prompt"}]
-    }]
-}""".trimIndent(),
-        ).body()
+            body = body,
+        ).toType()
     }
 }
 
+@Serializable
+data class RequestBody(
+    val contents: List<Content>? = null,
+)
 
 @Serializable
 data class Result(
